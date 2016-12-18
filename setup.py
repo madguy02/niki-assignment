b396b1f2bede3105491827e7ecb86673e7596279
madguy02 <manishkakoti688@gmail.com>
2016-12-18 18:50:15 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'niki',
+  packages = ['niki'], 
+  version = '0.1',
+  description = 'A recursive search library for phone numbers',
+  author = 'Manish Kakoti',
+  author_email = 'manishkakoti688@gmail.com',
+  url = 'https://github.com/madguy02/niki-assignment',
+  download_url = 'https://github.com/madguy02/niki-assignment/tarball/0.1', 
+  keywords = ['testing', 'logging', 'recursive search'], 
+  classifiers = [],
+)
