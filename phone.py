02e49b43624089892337a858e632e79874e30bbf
madguy02 <manishkakoti688@gmail.com>
2016-12-18 12:05:51 +0530

    changes

--- phone.py
+++ phone.py
@@ -1,9 +1,9 @@
 import re
 
-bible = open("my-directory-list.txt", "r")
+phone = open("my-directory-list.txt", "r")
 regex = re.compile(r'^[7896]\d{9}$')
 
-for line in bible:
-    four_letter_words = regex.findall(line)
-    for word in four_letter_words:
+for line in phone:
+    phone_numbers = regex.findall(line)
+    for word in phone_numbers:
         print word
