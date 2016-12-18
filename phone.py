6ace2c240f390f6cf919047c87af1cfc84610144
madguy02 <manishkakoti688@gmail.com>
2016-12-18 15:29:24 +0530

    import niki to phone

--- phone.py
+++ phone.py
@@ -1,7 +1,8 @@
 import re
+import niki
 
 phone = open("my-directory-list.txt", "r")
-regex = re.compile(r'^[7896]\d{9}$')
+regex = re.compile(r'^[7-9][0-9]{9}$')
 
 for line in phone:
     phone_numbers = regex.findall(line)
